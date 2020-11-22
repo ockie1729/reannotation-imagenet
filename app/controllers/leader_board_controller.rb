8aa4ea643531f9d40cd0980935d2a1c557c22395
mofumofuchan <you_oki1@yahoo.co.jp>
2020-11-22 17:39:28 +0900

    SQL文をpgにも対応できるよう修正

--- app/controllers/leader_board_controller.rb
+++ app/controllers/leader_board_controller.rb
@@ -2,7 +2,7 @@ class LeaderBoardController < ApplicationController
   def index
     @users = User.all.left_joins(:annotations)
                .group(:id)
-               .select('users.name, COUNT(`annotations`.`id`) as count_annotations_id')
+               .select('users.name, COUNT(annotations.id) as count_annotations_id')
                .order('count_annotations_id desc')
  end
 end
