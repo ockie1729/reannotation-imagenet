f3631f64b7d29c782ff51d63e818fa86cef8d148
mofumofuchan <you_oki1@yahoo.co.jp>
2020-12-02 23:46:04 +0900

    CoffeeScriptを削除

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,7 @@ module ProtoReannotationImagenet
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.time_zone = 'Tokyo'
+
+    config.generators.javascript_engine = :js
   end
 end
